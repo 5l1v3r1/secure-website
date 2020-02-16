19ac9799f0d7ffddb2dcc8349fef39e0b648ea84
Ivan Šincek <35937483+ivan-sincek@users.noreply.github.com>
2020-02-16 20:53:35 +0100

    Initial Commit

---
+++ src/php/sign_out.php
@@ -0,0 +1,11 @@
+<?php
+include_once './user.class.php';
+include_once './session.class.php';
+if (isset($_SERVER['REQUEST_METHOD']) && strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
+    if (Session::getUser()) {
+        Session::deleteSession();
+    }
+}
+header('Location: ../');
+exit();
+?>
